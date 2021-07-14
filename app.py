4717e03544655e79b8583003a2fd56d17eac1984
Jonathan <jsuconota1@gmail.com>
2021-07-14 15:47:38 +0000

    Update 11:47 7/14

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+from model import pokemon_choice_message
 
 
 # -- Initialization section --
@@ -12,5 +13,26 @@ app = Flask(__name__)
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
+
 def index():
-    return "hello world"
+    props = {
+        'name': 'Blaziken',
+        'starts-as': 'Torchic',
+        'type': 'fire & fighting'
+    }
+    return render_template('index.html', props=props)
+
+#get 
+#post
+
+@app.route('/secret')
+def secret():
+    return "You found Mew"
+
+@app.route('/result', methods = ["GET", "POST"])
+def result():
+    print(request.form["pokemon"])
+    user_choice = request.form["pokemon"]
+    message = pokemon_choice_message(user_choice)
+    return render_template('resultspage.html', message = message)
+
---
+++ model.py
@@ -0,0 +1,5 @@
+def pokemon_choice_message(user_choice):
+    if  user_choice == "Blaziken":
+        return "Your correct"
+    elif user_choice != "Blaziken":
+        return " Fix your answer" 
